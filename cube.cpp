db6085466a88e42d68b5e3e6e203743a0dfb61cb
Shobhit Kumar <kumar@shobhit.info>
2017-06-02 14:39:05 +0530

    First basic rotating cube with texture applied
    still overwriting triangles makes it look a lil odd. Did general cleanup
    removing color which was not needed. Also used only element array to
    draw the cube
    
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- cube.cpp
+++ cube.cpp
@@ -3,7 +3,7 @@
  * File Name : cube.cpp
  * Purpose :
  * Creation Date : 27-04-2017
- * Last Modified : Thursday 01 June 2017 09:15:35 PM IST
+ * Last Modified : Friday 02 June 2017 02:38:42 PM IST
  * Created By : Shobhit Kumar <kumar@shobhit.info>
  *
  * Code heavily borrowed from https://learnopengl.com
@@ -28,19 +28,56 @@
 #define TEXTURE1_IMAGE_FILE	"textures/awesomeface.png"
 
 static GLfloat vertices[] = {
-	// only unique veritices for square
-	// Positions			// Color			// texture coordinates
-	-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	0.0f, 0.0f,				// left bottom
-	-0.5f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	0.0f, 1.0f,				// left top
-	0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f,				// right bottom
-	0.5f, 0.5f, 0.0f,		1.0f, 1.0f, 0.0f,	1.0f, 1.0f,				// right top
+	-0.5f,  0.5f, -0.5f, 0.0f, 0.0f, // 0
+	 0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // 1
+	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, // 2
+	-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // 3
+
+	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // 4
+	 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // 5
+	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, // 6
+	-0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // 7
+
+	-0.5f,  0.5f,  0.5f, 0.0f, 0.0f, // 8
+	 0.5f,  0.5f,  0.5f, 0.0f, 1.0f, // 9
+	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, // 10
+	-0.5f, -0.5f,  0.5f, 1.0f, 0.0f, // 11
+
+	-0.5f,  0.5f, -0.5f, 0.0f, 0.0f, // 12
+	 0.5f,  0.5f, -0.5f, 0.0f, 1.0f, // 13
+	 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, // 14
+	-0.5f, -0.5f, -0.5f, 1.0f, 0.0f, // 15
+
+	-0.5f,  0.5f, -0.5f, 0.0f, 0.0f, // 16
+	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // 17
+	-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, // 18
+	-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // 19
+
+	 0.5f,  0.5f, -0.5f, 0.0f, 0.0f, // 20
+	 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // 21
+	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, // 22
+	 0.5f,  0.5f,  0.5f, 1.0f, 0.0f, // 23
 };
 
 static GLuint indices [] = {
-	// first triangle
-	0, 1, 2,
-	1, 2, 3
-	// second triangle
+	// Top Face
+	0, 1, 3,
+	1, 2, 3,
+	// bottom Face
+	4, 5, 7,
+	5, 6, 7,
+	// Front face
+	8, 9, 11,
+	9, 10, 11,
+	// Back Face
+	12, 13, 15,
+	13, 14, 15,
+	// Left face
+	16, 17, 19,
+	17, 18, 19,
+	// Right Face
+	20, 21, 23,
+	21, 22, 23
 };
 
 int simple_cube(GLFWwindow* window, int width, int height)
@@ -75,12 +112,10 @@ int simple_cube(GLFWwindow* window, int width, int height)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
 	glBindVertexArray(0);
 
 	// Load and create texture
@@ -112,7 +147,7 @@ int simple_cube(GLFWwindow* window, int width, int height)
 
 		// Translate to world co-ordinates
 		glm::mat4 model;
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(40.0f), glm::vec3(1.0f, 0.5f, 0.0f));
 
 		// View matrix
 		glm::mat4 view;
@@ -132,7 +167,7 @@ int simple_cube(GLFWwindow* window, int width, int height)
 		int projectionLoc = glGetUniformLocation(shaderProgram->get_id(), "projection");
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
 		glBindVertexArray(0);
 
